af0a59f6db52a516aa93f709b81158275abfa96c
Josh White <whitejosh429@gmail.com>
2024-11-19 23:00:22 +0000

    base

---
+++ public/bar/nav.php
@@ -0,0 +1,3 @@
+<div class="nav">
+    <a class="button" href="index.php">Home</a>
+</div>
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<head>
+    <title>Home</title>
+    <?php include "bar/nav.php" ?> 
+    <link rel="stylesheet" href="/public/css/base.css">
+    <link rel="stylesheet" href="/public/css/nav.css">
+</head>
+<body>
+    <div class="main"><p>test</p></div>
+    
+</body>
+</html>
