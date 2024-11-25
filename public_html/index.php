958e7b6269618e7f0b1c618a7e6e2ef2be024cf3
Josh White <whitejosh429@gmail.com>
2024-11-25 23:13:04 +0000

    test

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="css/nav.css">
 </head>
 <body>
-    <div class="main"><p>test update</p></div>
+    <div class="main"><p>test updates</p></div>
     
 </body>
 </html>
