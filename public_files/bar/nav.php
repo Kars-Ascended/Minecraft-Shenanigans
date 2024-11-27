c1d05f253cb94f6d7c3f30f5c0fd189b36dee87f
Josh White <whitejosh429@gmail.com>
2024-11-27 00:36:59 +0000

    big progress

--- public_files/bar/nav.php
+++ public_files/bar/nav.php
@@ -1,3 +1,5 @@
 <div class="nav">
-    <a class="button" href="index.php">Home</a>
+    <a class="button" href="index_real.php">Home</a>
+    <a class="button" href="status.php">Status</a>
+    <a class="button" href="server_info.php">Server Info</a>
 </div>
---
+++ public_files/server_info.php
@@ -0,0 +1,59 @@
+<head>
+    <title>Servers</title>
+    <?php include "bar/nav.php" ?> 
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/flex+box.css">
+    <link rel="stylesheet" href="css/nav.css">
+</head>
+<body>
+    <div class="main">
+        <h1>Servers</h1>
+        <br>
+        <div class="flex">
+            
+            <div class="box srv1">
+                <h1 class="centre">Vanilla+</h1>
+                <p>A mostly vanilla survival server with a few additons such as quark, me telling you about the mod will not do it justice so i suggest you check it out yourself, your bound to find something in there that you love</p>
+                <p>server will open soon</p>
+                <div class="server_buttons">
+                    <p class="button">IP: Soon</p>
+                    <p class="button">Version: 1.20.1</p>
+                    <p class="button">Modloader: Forge</p>
+                    <p class="button">Mods: <a href="https://google.com">Soon</a></p>
+                </div>
+            </div>
+
+            <div class="box srv2"><h1 class="centre">???</h1>
+                <p>???</p>
+                <div class="server_buttons">
+                    <p class="button">IP: ???</p>
+                    <p class="button">Version: ???</p>
+                    <p class="button">Modloader: ???</p>
+                    <p class="button">Mods: <a href="https://google.com">???</a></p>
+                </div>
+            </div>
+
+            <div class="box srv3"><h1 class="centre">???</h1>
+                <p>???</p>
+                <div class="server_buttons">
+                    <p class="button">IP: ???</p>
+                    <p class="button">Version: ???</p>
+                    <p class="button">Modloader: ???</p>
+                    <p class="button">Mods: <a href="https://google.com">???</a></p>
+                </div>
+            </div>
+
+            <div class="box srv4"><h1 class="centre">???</h1>
+                <p>???</p>
+                <div class="server_buttons">
+                    <p class="button">IP: ???</p>
+                    <p class="button">Version: ???</p>
+                    <p class="button">Modloader: ???</p>
+                    <p class="button">Mods: <a href="https://google.com">???</a></p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
