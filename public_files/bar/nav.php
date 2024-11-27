6324b807c0d309c1efc2a29e9c2eb96d6d19a0dc
Josh White <whitejosh429@gmail.com>
2024-11-27 00:42:24 +0000

    css changes

--- public_files/bar/nav.php
+++ public_files/bar/nav.php
@@ -1,5 +1,5 @@
 <div class="nav">
-    <a class="button" href="index_real.php">Home</a>
-    <a class="button" href="status.php">Status</a>
-    <a class="button" href="server_info.php">Server Info</a>
+    <a class="button" href="home">Home</a>
+    <a class="button" href="status">Status</a>
+    <a class="button" href="servers">Server Info</a>
 </div>
